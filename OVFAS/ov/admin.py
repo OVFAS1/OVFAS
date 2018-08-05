f032b88b6e745c64e815ee6b8c949306ffcdd9d3
Shashwat Sanket <shashwatsanket997@gmail.com>
2018-08-06 00:58:43 +0530

    ss

--- OVFAS/ov/admin.py
+++ OVFAS/ov/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from ov.models import HostelBlock
+from ov.models import HostelBlock,OutingForm
 # Register your models here.
 admin.site.register(HostelBlock)
+admin.site.register(OutingForm)
--- OVFAS/ov/models.py
+++ OVFAS/ov/models.py
@@ -10,4 +10,22 @@ class HostelBlock(models.Model):
     def __str__(self):
         return(self.block_name)
 
+class OutingForm(models.Model):
+    visibility=models.BooleanField(default=False)
+    registration_no=models.CharField(max_length=9)
+    name=models.CharField(max_length=100)
+    mess_type=models.CharField(max_length=100)
+    phone_regex = RegexValidator(regex=r'^\d{10}$', message="Phone number should be up to 10 digits only")
+    student_phone_number=models.CharField(validators=[phone_regex],max_length=13,default='')
+    parent_phone_number=models.CharField(validators=[phone_regex],max_length=13,default='')
+    hostel_block=models.ForeignKey(HostelBlock,on_delete=models.SET_NULL,null=True)
+    time_of_leaving=models.TimeField(blank=False)
+    time_of_arrival=models.TimeField(blank=False)
+    viiting_address=models.TextField()
+    purpose_of_leaving=models.CharField(max_length=250)
+    permanent_address=models.TextField()
+    def __str__(self):
+        return(self.registration_no)
+
+
 
